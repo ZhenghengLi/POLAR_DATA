286bafd1adc115d19373dff1461a3bf75f901cf3
Zhengheng Li <zhenghenge@gmail.com>
2015-11-07 19:52:29 +0800

    add the neccesary method to class EventMerger

--- include/EventMerger.hpp
+++ include/EventMerger.hpp
@@ -6,9 +6,10 @@
 #include <iomanip>
 #include <stdint.h>
 #include <vector>
+#include <queue>
+#include <algorithm>
 #include "SciTrigger.hpp"
 #include "SciEvent.hpp"
-#include "trigger_queue.hpp"
 
 using namespace std;
 
@@ -19,15 +20,48 @@ private:
 	SciTrigger result_noped_trigger_;
 	vector<SciEvent> result_noped_events_vec_;
 
+	SciTrigger curr_ped_trigger_;
+	SciTrigger next_ped_trigger_;
+	vector<SciEvent> curr_ped_events_vec_;
+	vector<int> curr_ped_event_IDs_vec_;
+	
+	bool ped_trigger_is_first_;
+	bool ped_event_is_first_[25];
+	bool noped_trigger_is_first_;
+	bool noped_event_is_first_[25];
+
+	int trigger_period_;
+	int trigger_pre_time_align_;
+	int event_period_[25];
+	int event_pre_time_align_[25];
+
+	int global_time_diff_;
+	bool global_start_flag_;
+
+	priority_queue<SciTrigger, vector<SciTrigger>, greater<SciTrigger> > noped_trigger_queue_;
+	priority_queue<SciEvent, vector<SciEvent>, greater<SciEvent> > noped_event_queue_[25];
+private:
+	bool can_noped_do_merge() const;
 public:
+	EventMerger();
+	~EventMerger();
+
 	const SciTrigger& get_result_ped_trigger();
 	const vector<SciEvent>& get_result_ped_events_vec();
 	const SciTrigger& get_result_noped_trigger();
 	const vector<SciEvent>& get_result_noped_events_vec();
-	void add_ped_trigger(SciTrigger& trigger);
-	void add_ped_event(SciEvent& event);
-	void add_noped_trigger(SciTrigger& trigger);
-	void add_noped_event(SciEvent& event);
+
+	void add_ped_trigger(const SciTrigger& trigger);
+	void add_ped_event(const SciEvent& event);
+	void add_noped_trigger(const SciTrigger& trigger);
+	void add_noped_event(const SciEvent& event);
+
+	bool noped_do_merge();
+	void noped_clear_result();
+	bool ped_check_valid() const ;
+	void ped_update_time_diff();
+	void ped_move_result(bool valid);
+	void ped_clear_result();
 };
 
 #endif
--- src/EventMerger.cpp
+++ src/EventMerger.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+EventMerger::EventMerger() {
+	ped_trigger_is_first_ = true;
+	noped_trigger_is_first_ = true;
+	for (int i = 0; i < 25; i++) {
+		ped_event_is_first_[i] = true;
+		noped_event_is_first_[i] = true;
+	}
+	global_start_flag_ = false;
+}
+
+EventMerger::~EventMerger() {
+
+}
+
 const SciTrigger& EventMerger::get_result_ped_trigger() {
 	return result_ped_trigger_;
 }
@@ -18,18 +32,46 @@ const vector<SciEvent>& EventMerger::get_result_noped_events_vec() {
 	return result_noped_events_vec_;
 }
 
-void EventMerger::add_ped_trigger(SciTrigger& trigger) {
+void EventMerger::add_ped_trigger(const SciTrigger& trigger) {
 
 }
 
-void EventMerger::add_ped_event(SciEvent& event) {
+void EventMerger::add_ped_event(const SciEvent& event) {
 
 }
 
-void EventMerger::add_noped_trigger(SciTrigger& trigger) {
+void EventMerger::add_noped_trigger(const SciTrigger& trigger) {
 
 }
 
-void EventMerger::add_noped_event(SciEvent& event) {
+void EventMerger::add_noped_event(const SciEvent& event) {
 
+}
+
+bool EventMerger::noped_do_merge() {
+	return true;
+}
+
+void EventMerger::noped_clear_result() {
+
+}
+
+bool EventMerger::ped_check_valid() const {
+	return true;
+}
+
+void EventMerger::ped_update_time_diff() {
+
+}
+
+void EventMerger::ped_move_result(bool valid) {
+
+}
+
+void EventMerger::ped_clear_result() {
+
+}
+
+bool EventMerger::can_noped_do_merge() const {
+	return true;
 }
