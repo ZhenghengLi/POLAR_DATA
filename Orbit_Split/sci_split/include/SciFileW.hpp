0f29107bc3e1a380114126a8394855e48b4759ba
Zhengheng Li <zhenghenge@gmail.com>
2016-06-08 14:42:43 +0800

    start SciFileW

--- Orbit_Split/sci_split/include/SciFileW.hpp
+++ Orbit_Split/sci_split/include/SciFileW.hpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <stdio.h>
 #include "RootInc.hpp"
 #include "SciType1P.hpp"
+#include "SciFileR.hpp"
 
 using namespace std;
 
 class SciFileW: private SciType1P {
+private:
+    TFile* t_file_out_;
+    TTree* t_modules_tree_;
+    TTree* t_trigger_tree_;
+    TTree* t_ped_modules_tree_;
+    TTree* t_ped_trigger_tree_;
 
+    SciFileR* cur_scifile_r;
+    Long64_t  cur_phy_trigg_num_offset_;
+    Long64_t  cur_ped_trigg_num_offset_;
+    Long64_t  cur_phy_event_num_offset_[25];
+    Long64_t  cur_ped_event_num_offset_[25];
+    
+    Long64_t  cur_trigg_num_g_offset_;
+    Long64_t  cur_event_num_g_offset_[25];
+
+    UInt_t    phy_trigger_last_time_stamp_;
+    UInt_t    ped_trigger_last_time_stamp_;    
+    UInt_t    phy_modules_last_time_stamp_[25];
+    UInt_t    ped_modules_last_time_stamp_[25];
+    
+
+public:
+    Modules_T t_modules;
+    Trigger_T t_trigger;
+    Modules_T t_ped_modules;
+    Trigger_T t_ped_trigger;
+    
+public:
+    SciFileW();
+    ~SciFileW();
+
+    bool open(const char* filename);
+    void close();
+
+    void set_scifile_r(SciFileR* scifile_r);
 
 };
--- Orbit_Split/sci_split/src/SciFileW.cpp
+++ Orbit_Split/sci_split/src/SciFileW.cpp
@@ -1,1 +1,64 @@
 #include "SciFileW.hpp"
+
+SciFileW::SciFileW() {
+    t_file_out_ = NULL;
+    t_modules_tree_ = NULL;
+    t_trigger_tree_ = NULL;
+    t_ped_modules_tree_ = NULL;
+    t_ped_trigger_tree_ = NULL;
+
+    cur_scifile_r = NULL;
+}
+
+SciFileW::~SciFileW() {
+    close();
+}
+
+
+bool SciFileW::open(const char* filename) {
+    if (t_file_out_ != NULL)
+        return false;
+    
+    t_file_out_ = new TFile(filename, "RECREATE");
+    if (t_file_out_->IsZombie())
+        return false;
+
+    // t_modules
+    t_modules_tree_ = new TTree("t_modules", "physical modules packets");
+    build_modules_tree(t_modules_tree_, t_modules);
+
+    // t_trigger
+    t_trigger_tree_ = new TTree("t_trigger", "physical trigger packets");
+    build_trigger_tree(t_trigger_tree_, t_trigger);
+
+    // t_ped_modules
+    t_ped_modules_tree_ = new TTree("t_ped_modules", "pedestal modules packets");
+    build_modules_tree(t_ped_modules_tree_, t_ped_modules);
+
+    // t_ped_trigger
+    t_ped_trigger_tree_ = new TTree("t_ped_trigger", "pedestal trigger packets");
+    build_trigger_tree(t_ped_trigger_tree_, t_ped_trigger);
+
+    return true;
+}
+
+void SciFileW::close() {
+    if (t_file_out_ == NULL)
+        return;
+
+    delete t_modules_tree_;
+    t_modules_tree_ = NULL;
+
+    delete t_trigger_tree_;
+    t_trigger_tree_ = NULL;
+
+    delete t_ped_modules_tree_;
+    t_ped_modules_tree_ = NULL;
+
+    delete t_ped_trigger_tree_;
+    t_ped_trigger_tree_ = NULL;
+
+    t_file_out_->Close();
+    delete t_file_out_;
+    t_file_out_ = NULL;
+}
