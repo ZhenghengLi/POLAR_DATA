8b703573a481172dd02c3ee90867434fa133c9d1
Zhengheng Li <zhenghenge@gmail.com>
2016-07-13 19:39:54 +0800

    update version information

--- L1_Convert/hk_l1convert/include/Constants.hpp
+++ L1_Convert/hk_l1convert/include/Constants.hpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const string SW_NAME = "HK_L1Convert";
 
-const string SW_VERSION = "v0.0.1";
+const string SW_VERSION = "v1.0.0";
 
 const string RELEASE_DATE = "2016 Jun 24";
 
--- Orbit_Split/sci_split/include/Constants.hpp
+++ Orbit_Split/sci_split/include/Constants.hpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const string SW_NAME = "SCI_Split";
 
-const string SW_VERSION = "v0.0.1";
+const string SW_VERSION = "v1.0.0";
 
 const string RELEASE_DATE = "2016 Jun 10";
 
