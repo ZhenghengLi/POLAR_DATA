2d07ed4a0bbd15b01c564693f77d474bd655e3ac
Zhengheng Li <zhenghenge@gmail.com>
2016-08-06 19:42:41 +0800

    change of Makefile about Boost

--- env.sh
+++ env.sh
@@ -6,11 +6,18 @@ my_dir=$(cd $tmp_dir > /dev/null ; pwd)
 my_path=$my_dir/Analysis/bin:$my_dir/Analysis/script:$my_dir/Preprocessing/bin:$my_dir/Preprocessing/script:$my_dir/Tools/bin:$my_dir/Tools/script
 my_lib=$my_dir/Analysis/bin
 
+#boost_prefix=/opt/mark/Boost/Boost_1_61_0_ABI_0
+
 if [[ $PATH =~ $my_path ]]; then
     echo "POLAR_RawData_Decode_Project is already in PATH"
 else
     export PATH=$my_path:$PATH
-    export export LD_LIBRARY_PATH=$my_lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$my_lib:$LD_LIBRARY_PATH
+    if [[ -v boost_prefix ]]; then
+        export BOOST_INC=-I$boost_prefix/include
+        export BOOST_LIB=-L$boost_prefix/lib
+        export LD_LIBRARY_PATH=$boost_prefix/lib:$LD_LIBRARY_PATH
+    fi
     echo "Added POLAR_RawData_Decode_Project into PATH"
 fi
 
