c10d195caf9c59060de30f58a5ce6ae7c654eb9e
Zhengheng Li <zhenghenge@gmail.com>
2016-08-06 20:38:05 +0800

    change of Makefile: shift order

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ my_dir=$(cd $tmp_dir > /dev/null ; pwd)
 my_path=$my_dir/Analysis/bin:$my_dir/Analysis/script:$my_dir/Preprocessing/bin:$my_dir/Preprocessing/script:$my_dir/Tools/bin:$my_dir/Tools/script
 my_lib=$my_dir/Analysis/bin
 
-#boost_prefix=/opt/mark/Boost/Boost_1_61_0_ABI_0
+#boost_prefix=/opt/mark/Boost/Boost_1_61_0_old_ABI
 
 if [[ $PATH =~ $my_path ]]; then
     echo "POLAR_RawData_Decode_Project is already in PATH"
