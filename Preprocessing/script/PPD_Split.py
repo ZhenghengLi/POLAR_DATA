0ace74edbd0a614ac0b0cf358334da5ac1499793
Zhengheng Li <zhenghenge@gmail.com>
2016-08-30 23:45:19 +0800

    nearly finish ppd_file_r.py, need testing

--- Preprocessing/script/PPD_Split.py
+++ Preprocessing/script/PPD_Split.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='Split platfrom parameters data by 
 parser.add_argument('filelist', metavar = 'filename', nargs = '+', help = 'list of filenames to open')
 parser.add_argument('-B', dest = 'begin', help = 'utc time of beginning as gps time form, like utc_week:utc_second', default = 'begin')
 parser.add_argument('-E', dest = 'end'  , help = 'utc time of ending as gps time form, like utc_week:utc_second', default = 'end')
-parser.add_argument('-o', dest = 'outfile', help = 'ROOT file to stored splitted platform parameters data', default = 'TG2_PPD_file_split.root')
+parser.add_argument('-o', dest = 'outfile', help = 'ROOT file to store splitted platform parameters data', default = 'TG2_PPD_file_split.root')
 args = parser.parse_args()
 
 print args.filelist
--- Preprocessing/script/ppd_file_r.py
+++ Preprocessing/script/ppd_file_r.py
@@ -1,15 +1,76 @@
 #!/usr/bin/env python
 
+import re
 from rootpy import ROOT
 from rootpy.io import File
 from rootpy.tree import Tree
 
+MIN_DIFF = 5
+
 class ppd_file_r:
     def __init__(self):
-        self.__t_file_in  = None
-        self.__t_tree_ppd = None
-        self.__begin_entry = 0
-        self.__end_entry   = 0
+        self.t_file_in             = None
+        self.t_tree_ppd            = None
+        self.begin_entry           = 0
+        self.end_entry             = 0
+        self.utc_time_span         = ''
+        self.first_utc_time_sec  = 0.0
+        self.last_utc_time_sec   = 0.0
+        self.begin_utc_time_sec  = 0.0
+        self.end_utc_time_sec    = 0.0
+
+    def __find_entry(self, utc_time_sec):
+        head_entry = 0
+        tail_entry = self.t_tree_ppd.get_entries() - 1
+        while tail_entry - head_entry > 1:
+            center_entry = int((head_entry + tail_entry) / 2)
+            self.t_tree_ppd.get_entry(center_entry)
+            if utc_time_sec == self.t_tree_ppd.utc_time_sec:
+                return center_entry
+            else if utc_time_sec > self.t_tree_ppd.utc_time_sec:
+                head_entry = center_entry
+            else:
+                tail_entry = center_entry
+        return head_entry
 
     def open_file(self, filename, begin, end):   # cut data by utc time, utc_week:utc_second
-        print "open"
+        self.t_file_in = File(filename, 'read')
+        self.t_tree_ppd = self.t_file_in.get('t_ppd')
+        self.t_tree_ppd.create_buffer()
+        self.utc_time_span = self.t_file_in.get('utc_time_span').GetTitle()
+        m = re.compile(r'(\d+):(\d+)\[\d+\] => (\d+):(\d+)\[\d+\]; \d+/\d+').match(self.utc_time_span)
+        self.first_utc_time_sec = float(m.group(1)) * 604800 + float(m.group(2))
+        self.last_utc_time_sec  = float(m.group(3)) * 604800 + float(m.group(4))
+        if begin != 'begin':
+            m = re.compile(r'(\d+):(\d+)').match(begin)
+            self.begin_utc_time_sec = float(m.group(1)) * 604800 + float(m.group(2))
+            if self.begin_utc_time_sec - self.first_utc_time_sec < MIN_DIFF:
+                print 'WARNING: begin utc time is out of range: ' + str(self.begin_utc_time_sec - self.first_utc_time_sec )
+                return False
+        else:
+            self.begin_utc_time_sec = -1
+        if end != 'end':
+            m = re.compile(r'(\d+):(\d+)').match(end)
+            self.end_utc_time_sec = float(m.group(1)) * 604800 + float(m.group(2))
+            if self.last_utc_time_sec - self.end_utc_time_sec < MIN_DIFF:
+                print 'WARNING: end utc time is out of range: ' + str(self.last_utc_time_sec - self.end_utc_time_sec)
+                return False
+        else:
+            self.end_utc_time_sec = -1
+        if self.begin_utc_time_sec > 0 and self.end_utc_time_sec > 0 and self.end_utc_time_sec - self.begin_utc_time_sec < MIN_DIFF:
+            print 'WARNING: time span between begin and end utc time is too small: ' + str(self.end_utc_time_sec - self.begin_utc_time_sec)
+            return False
+        if self.begin_utc_time_sec > 0:
+            self.begin_entry = self.__find_entry(self.begin_utc_time_sec)
+        else:
+            self.begin_entry = 0
+        if self.end_utc_time_sec > 0:
+            self.end_entry = self.__find_entry(self.end_utc_time_sec)
+        else:
+            self.end_entry = self.t_tree_ppd.get_entries()
+
+    def close_file():
+        self.t_file_in.close()
+        self.t_file_in = None
+        self.t_tree_ppd  = None
+
