a2199914e8b95f22dfa13502193aaf1bbce21c77
Zhengheng Li <zhenghenge@gmail.com>
2016-10-08 13:16:34 +0800

    add print_location.py

---
+++ Analysis/script/ppd_file_r.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+import re
+from os.path import basename
+from rootpy import ROOT
+from rootpy.io import File
+from rootpy.tree import Tree
+
+_MIN_DIFF = 5
+
+class ppd_file_r:
+    def __init__(self):
+        self.t_file_name           = ''
+        self.t_file_in             = None
+        self.t_tree_ppd            = None
+        self.begin_entry           = 0
+        self.end_entry             = 0
+        self.utc_time_span         = ''
+        self.first_utc_time_sec    = 0.0
+        self.last_utc_time_sec     = 0.0
+        self.begin_utc_time_sec    = 0.0
+        self.end_utc_time_sec      = 0.0
+
+    def __find_entry(self, utc_time_sec):
+        head_entry = -1
+        head_entry_found = False
+        while head_entry < self.t_tree_ppd.get_entries():
+            head_entry += 1
+            self.t_tree_ppd.get_entry(head_entry)
+            if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0:
+                head_entry_found = True
+                break
+        if not head_entry_found:
+            return -1
+        if utc_time_sec < self.t_tree_ppd.utc_time_sec:
+            return -1
+        tail_entry = self.t_tree_ppd.get_entries()
+        tail_entry_found = False
+        while tail_entry >= 0:
+            tail_entry -= 1
+            self.t_tree_ppd.get_entry(tail_entry)
+            if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0:
+                tail_entry_found = True
+                break
+        if not tail_entry_found:
+            return -1
+        if utc_time_sec > self.t_tree_ppd.utc_time_sec:
+            return -1
+        while tail_entry - head_entry > 1:
+            center_entry = int((head_entry + tail_entry) / 2)
+            found_valid_center = False
+            self.t_tree_ppd.get_entry(center_entry)
+            if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0:
+                found_valid_center = True
+            tmp_center_entry = center_entry
+            while not found_valid_center and tail_entry - tmp_center_entry > 1:
+                tmp_center_entry += 1
+                self.t_tree_ppd.get_entry(tmp_center_entry)
+                if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0:
+                    found_valid_center = True
+            if not found_valid_center: tmp_center_entry = center_entry
+            while not found_valid_center and tmp_center_entry - head_entry > 1:
+                tmp_center_entry -= 1
+                self.t_tree_ppd.get_entry(tmp_center_entry)
+                if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0:
+                    found_valid_center = True
+            if not found_valid_center: break
+            if utc_time_sec == self.t_tree_ppd.utc_time_sec:
+                return tmp_center_entry
+            elif utc_time_sec > self.t_tree_ppd.utc_time_sec:
+                head_entry = tmp_center_entry
+            else:
+                tail_entry = tmp_center_entry
+        return tail_entry
+
+    def open_file(self, filename, begin, end):   # cut data by utc time, utc_week:utc_second
+        self.t_file_name = basename(filename)
+        self.t_file_in = File(filename, 'read')
+        self.t_tree_ppd = self.t_file_in.get('t_ppd')
+        self.t_tree_ppd.create_buffer()
+        self.utc_time_span = self.t_file_in.get('m_utc_span').GetTitle()
+        m = re.compile(r'(\d+):(\d+)\[\d+\] => (\d+):(\d+)\[\d+\]; \d+/\d+').match(self.utc_time_span)
+        self.first_utc_time_sec = float(m.group(1)) * 604800 + float(m.group(2))
+        self.last_utc_time_sec  = float(m.group(3)) * 604800 + float(m.group(4))
+        if begin != 'begin':
+            m = re.compile(r'(\d+):(\d+)').match(begin)
+            self.begin_utc_time_sec = float(m.group(1)) * 604800 + float(m.group(2))
+            if self.begin_utc_time_sec - self.first_utc_time_sec < _MIN_DIFF:
+                print 'WARNING: begin utc time is out of range: ' + str(self.begin_utc_time_sec - self.first_utc_time_sec )
+                return False
+        else:
+            self.begin_utc_time_sec = -1
+        if end != 'end':
+            m = re.compile(r'(\d+):(\d+)').match(end)
+            self.end_utc_time_sec = float(m.group(1)) * 604800 + float(m.group(2))
+            if self.last_utc_time_sec - self.end_utc_time_sec < _MIN_DIFF:
+                print 'WARNING: end utc time is out of range: ' + str(self.last_utc_time_sec - self.end_utc_time_sec)
+                return False
+        else:
+            self.end_utc_time_sec = -1
+        if self.begin_utc_time_sec > 0 and self.end_utc_time_sec > 0 and self.end_utc_time_sec - self.begin_utc_time_sec < _MIN_DIFF:
+            print 'WARNING: time span between begin and end utc time is too small: ' + str(self.end_utc_time_sec - self.begin_utc_time_sec)
+            return False
+        if self.begin_utc_time_sec > 0:
+            self.begin_entry = self.__find_entry(self.begin_utc_time_sec)
+            if self.begin_entry < 0:
+                print "WARNING: cannot find begin entry."
+                return False
+        else:
+            self.begin_entry = 0
+        if self.end_utc_time_sec > 0:
+            self.end_entry = self.__find_entry(self.end_utc_time_sec)
+            if self.end_entry < 0:
+                print "WARNING: cannot find end entry."
+                return False
+        else:
+            self.end_entry = self.t_tree_ppd.get_entries()
+        return True
+
+    def print_file_info(self):
+        actual_start_entry = 0
+        for idx in xrange(self.begin_entry, self.end_entry):
+            actual_start_entry = idx
+            self.t_tree_ppd.get_entry(idx)
+            if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0: break
+        actual_begin_utc_time_sec = self.t_tree_ppd.utc_time_sec
+        actual_end_entry = 0
+        for idx in xrange(self.end_entry - 1, self.begin_entry - 1, -1):
+            actual_end_entry = idx
+            self.t_tree_ppd.get_entry(idx)
+            if self.t_tree_ppd.flag_of_pos == 0x55 and self.t_tree_ppd.utc_time_sec > 0: break
+        actual_end_utc_time_sec   = self.t_tree_ppd.utc_time_sec
+        utc_time_span_str = '%d:%d[%d] => %d:%d[%d]' % (int(actual_begin_utc_time_sec / 604800),
+                                                        int(actual_begin_utc_time_sec % 604800),
+                                                        actual_start_entry,
+                                                        int(actual_end_utc_time_sec / 604800),
+                                                        int(actual_end_utc_time_sec % 604800),
+                                                        actual_end_entry)
+        print self.t_file_name
+        print ' - UTC time span: { ' + utc_time_span_str + ' }'
+
+    def close_file(self):
+        self.t_file_in.close()
+        self.t_file_in = None
+        self.t_tree_ppd  = None
+
---
+++ Analysis/script/print_location.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+SW_NAME      = 'PPD_Split.py'
+SW_VERSION   = 'v1.0.0'
+RELEASE_DATE = '2016 Sep  5'
+
+import argparse
+from os.path import basename
+from ppd_file_r import ppd_file_r
+from tqdm import tqdm
+import numpy as np
+import ephem as ep
+from datetime import datetime
+
+UTCStartTime = 1157984027.3750000000
+
+time_fmt_in  = '%Y-%m-%dT%H:%M:%S+0000'
+time_fmt_out = '%Y-%m-%d %H:%M:%S'
+
+def _xyz_to_latlon(x, y, z):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    return [90 - np.arccos(z / r) / np.pi * 180, np.arctan2(y, x) / np.pi * 180]
+
+def _wgs84_to_j2000(wgs84_xyz, utc):
+    lat, lon = _xyz_to_latlon(*wgs84_xyz)
+    location = ep.Observer()
+    location.lat  = lat / 180 * np.pi
+    location.lon  = lon / 180 * np.pi
+    location.date = utc
+    ra_now  = location.sidereal_time()
+    dec_now = lat / 180 * np.pi
+    pos_now   = ep.Equatorial(ra_now, dec_now, epoch = utc)
+    pos_j2000 = ep.Equatorial(pos_now, epoch = ep.J2000)
+    return [float(pos_j2000.ra) / np.pi * 12, float(pos_j2000.dec) / np.pi * 180]
+
+def _radec_to_j2000xyz(ra, dec, d):
+    theta = ra / 12 * np.pi
+    pha = (90 - dec) / 180 * np.pi
+    return (d * np.sin(theta) * np.cos(pha), d * np.sin(theta) * np.sin(pha), d * np.cos(theta)) 
+
+parser = argparse.ArgumentParser(description='print location and time')
+parser.add_argument('filename', help = 'decoded file to open')
+parser.add_argument('-B', dest = 'begin', help = 'utc time of beginning as gps time form, like utc_week:utc_second', default = 'begin')
+parser.add_argument('-E', dest = 'end'  , help = 'utc time of ending as gps time form, like utc_week:utc_second', default = 'end')
+parser.add_argument('-o', dest = 'outfile', help = 'text file to store location and time', default = 'loc_output.csv')
+args = parser.parse_args()
+
+ppd_file_r_obj = ppd_file_r()
+if not ppd_file_r_obj.open_file(args.filename, args.begin, args.end):
+    print 'Error: root file open failed: ' + args.filename
+    exit(1)
+
+ppd_file_r_obj.print_file_info()
+
+fout = open(args.outfile, 'w')
+fout.write("utc_time_second, j2000_x, j2000_y, j2000_z\n")
+
+for i in tqdm(xrange(ppd_file_r_obj.begin_entry, ppd_file_r_obj.end_entry)):
+    ppd_file_r_obj.t_tree_ppd.get_entry(i)
+    cur_time = ppd_file_r_obj.t_tree_ppd.utc_time_sec - UTCStartTime
+    wgs84_xyz = (ppd_file_r_obj.t_tree_ppd.wgs84_x, ppd_file_r_obj.t_tree_ppd.wgs84_y, ppd_file_r_obj.t_tree_ppd.wgs84_z)
+    utc = datetime.strptime(str(ppd_file_r_obj.t_tree_ppd.utc_time_str), time_fmt_in).strftime(time_fmt_out)
+    ra, dec = _wgs84_to_j2000(wgs84_xyz, utc)
+    j2000_xyz = _radec_to_j2000xyz(ra, dec, ppd_file_r_obj.t_tree_ppd.geocentric_d)
+    fout.write(str(cur_time) + ', ' + str(j2000_xyz[0]) + ', ' + str(j2000_xyz[1]) + ', ' + str(j2000_xyz[2]) + '\n')
+
+fout.close()
+
