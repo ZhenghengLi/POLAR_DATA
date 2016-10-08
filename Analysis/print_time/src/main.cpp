90567adc15e5384f0fa97fd02c75d2d6e7008016
Zhengheng Li <zhenghenge@gmail.com>
2016-10-08 15:25:50 +0800

    replace ', ' to ' '

--- Analysis/print_time/src/main.cpp
+++ Analysis/print_time/src/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char** argv) {
             continue;
         }
 		double cur_seconds = (eventIter.t_trigger.abs_gps_week - GPSStartWeek) * 604800 + (eventIter.t_trigger.abs_gps_second - GPSStartSecond);
-		outfile << lexical_cast<string>(cur_seconds) << ", " << eventIter.t_trigger.type << ", " << eventIter.t_trigger.trigger_n << endl;
+		outfile << lexical_cast<string>(cur_seconds) << " " << eventIter.t_trigger.type << " " << eventIter.t_trigger.trigger_n << endl;
     }
     cout << " DONE ]" << endl;
     eventIter.close();
