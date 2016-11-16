657d1a3cab154b11476f74747d7ffc954765f483
Zhengheng Li <zhenghenge@gmail.com>
2016-11-16 21:14:15 +0800

    add test code of SCIIterator

--- Preprocessing/SAP_Merge/src/main.cpp
+++ Preprocessing/SAP_Merge/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "OptionsManager.hpp"
+#include "SCIIterator.hpp"
 
 using namespace std;
 
@@ -18,6 +19,20 @@ int main(int argc, char** argv) {
     cout << options_mgr.auxfile.Data() << endl;
     cout << options_mgr.ppdfile.Data() << endl;
     cout << options_mgr.outfile.Data() << endl;
+
+    SCIIterator sciIter;
+    if (!sciIter.open(options_mgr.scifile.Data())) {
+        cout << "root file open faild." << endl;
+        return 1;
+    }
+
+    while (sciIter.next_event()) {
+        cout << sciIter.cur_trigger.trigg_num_g << " " << sciIter.cur_trigger.type << " | " << flush;
+        while(sciIter.next_packet()) {
+            cout << "(" << sciIter.cur_modules.ct_num << ", " << sciIter.cur_modules.compress << ") " << flush;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
