326de11c59957a4ede5fec5f027bc0937c7b85b9
Zhengheng Li <zhenghenge@gmail.com>
2016-11-30 20:54:34 +0800

    add draw_event_esrf

---
+++ Analysis_ESRF/draw_event_esrf/src/OptionsManager.cpp
@@ -0,0 +1,93 @@
+#include "OptionsManager.hpp"
+
+using namespace std;
+
+OptionsManager::OptionsManager() {
+    init();
+}
+
+OptionsManager::~OptionsManager() {
+
+}
+
+bool OptionsManager::parse(int argc_par, char** argv_par) {
+    if (argc_par < 2)
+        return false;
+    TString cur_par_str;
+    int idx = 0;
+    while (idx < argc_par - 1) {
+        cur_par_str = argv_par[++idx];
+        if (cur_par_str == "--version") {
+            version_flag_ = true;
+            return false;
+        }
+        if (cur_par_str[0] == '-') {
+            if (cur_par_str.Length() != 2)
+                return false;
+            char cur_option = cur_par_str[1];
+            switch (cur_option) {
+            case 'e':
+                if (idx < argc_par - 1) {
+                    TString tmp_string = argv_par[++idx];
+                    if (tmp_string[0] == '-') {
+                        return false;
+                    } else {
+                        entry = tmp_string.Atoi();
+                    }
+                } else {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+            }
+        } else {
+            if (event_data_filename.IsNull()) {
+                event_data_filename = cur_par_str;
+            } else {
+                return false;
+            }
+        }
+    }
+    if (event_data_filename.IsNull())
+        return false;
+    if (entry < 0)
+        return false;
+    return true;
+}
+
+void OptionsManager::print_help() {
+    cout << "Usage:" << endl;
+    cout << "  " << SW_NAME << " <event_data_file.root> -e <entry>" << endl;
+    cout << endl;
+    cout << "Options:" << endl;
+    cout << "  -e entry                         entry number, start from 0" << endl;
+    cout << endl;
+    cout << "  --version                        print version and author information" << endl;
+    cout << endl;
+}
+
+void OptionsManager::print_version() {
+    cout << endl;
+    cout << "    " << SW_NAME << " - POLAR Data Analysis Tools" << endl;
+    cout << "    " << SW_VERSION << " (" << RELEASE_DATE << ", compiled " << __DATE__ << " " << __TIME__ << ")" << endl;
+    cout << endl;
+    cout << " Copyright (C) 2015-2016 Zhengheng Li <lizhengheng@ihep.ac.cn>" << endl;
+    cout << " Source Code: https://github.com/ZhenghengLi/POLAR_RawData_Decode_Project" << endl;
+    cout << endl;
+}
+
+void OptionsManager::print_options() {
+
+}
+
+void OptionsManager::init() {
+    event_data_filename.Clear();
+    entry = -1;
+
+    version_flag_ = false;
+}
+
+bool OptionsManager::get_version_flag() {
+    return version_flag_;
+}
