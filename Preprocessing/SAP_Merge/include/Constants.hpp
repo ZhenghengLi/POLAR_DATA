694f3f30a841713339060fffa852d297ab00118b
Zhengheng Li <zhenghenge@gmail.com>
2016-12-05 13:32:12 +0800

    SAP_Merge, SAP_Split: change of MET start time

--- Preprocessing/SAP_Merge/include/Constants.hpp
+++ Preprocessing/SAP_Merge/include/Constants.hpp
@@ -7,15 +7,15 @@ using namespace std;
 
 const string SW_NAME = "SAP_Merge";
 
-const string SW_VERSION = "v1.2.0";
+const string SW_VERSION = "v1.2.1";
 
-const string RELEASE_DATE = "2016 Nov 25";
+const string RELEASE_DATE = "2016 Dec  5";
 
-const double METStartGPSTime = 1157984044.816;
+const double METStartGPSTime = 1157984045;
 const int    METStartGPSWeek = 1914;
-const double METStartGPSSecond = 396844.816;
-const string MET_UTC_REF_str = "2016-09-15 14:13:47.816 UTC";
-const string MET_TT_REF_str  = "1157984096.0";
-const string MET_MJD_REF_str = "57646.59291454";
+const double METStartGPSSecond = 396845;
+const string MET_UTC_REF_str = "2016-09-15 14:13:48 UTC";
+const string MET_TT_REF_str  = "1157984096.184";
+const string MET_MJD_REF_str = "57646.59291667";
 
 #endif
