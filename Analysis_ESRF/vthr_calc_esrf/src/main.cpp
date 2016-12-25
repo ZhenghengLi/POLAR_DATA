168f8137c3a2c57f9888564b2561f3b9c37154ab
Zhengheng Li <zhenghenge@gmail.com>
2016-12-25 15:24:07 +0800

    vthr_calc_esrf: output trigger ratio histogram

--- Analysis_ESRF/vthr_calc_esrf/src/main.cpp
+++ Analysis_ESRF/vthr_calc_esrf/src/main.cpp
@@ -62,13 +62,19 @@ int main(int argc, char** argv) {
     }
     t_file_ped_vec->Close();
 
+    TFile* t_file_out = new TFile(tcanvas_fn.c_str(), "recreate");
+    if (t_file_out->IsZombie()) {
+        cout << "root file of output open failed." << endl;
+        return 1;
+    }
+
     TH1F* tri_spec[25][64];
     TH1F* all_spec[25][64];
     TF1*  fun_spec[25][64];
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j < 64; j++) {
             tri_spec[i][j] = new TH1F(Form("tri_spec_%02d_%02d", i + 1, j + 1), Form("tri_spec_%02d_%02d", i + 1, j + 1), 256, -128, 1280);
-            tri_spec[i][j]->SetDirectory(NULL);
+            // tri_spec[i][j]->SetDirectory(NULL);
             all_spec[i][j] = new TH1F(Form("all_spec_%02d_%02d", i + 1, j + 1), Form("all_spec_%02d_%02d", i + 1, j + 1), 256, -128, 1280);
             all_spec[i][j]->SetDirectory(NULL);
             fun_spec[i][j] = new TF1(Form("fun_spec_%02d_%02d", i + 1, j + 1), "(TMath::Erf((x - [0]) / TMath::Sqrt(2) / [1]) + 1.0) / 2.0", -128, 1024);
@@ -132,11 +138,6 @@ int main(int argc, char** argv) {
     t_file_in = NULL;
 
     // write spec
-    TFile* t_file_out = new TFile(tcanvas_fn.c_str(), "recreate");
-    if (t_file_out->IsZombie()) {
-        cout << "root file of output open failed." << endl;
-        return 1;
-    }
     gROOT->SetBatch(kTRUE);
     gStyle->SetOptStat(11);
     gStyle->SetOptFit(111);
@@ -153,6 +154,7 @@ int main(int argc, char** argv) {
         vthr_adc_sigma[i].ResizeTo(64);
         vthr_adc_value_err[i].ResizeTo(64);
         vthr_adc_sigma_err[i].ResizeTo(64);
+        t_file_out->mkdir(Form("trigger_ratio_CT_%02d", i + 1))->cd();
         for (int j = 0; j < 64; j++) {
             canvas_spec[i]->cd(jtoc(j));
             canvas_spec[i]->GetPad(jtoc(j))->SetFillColor(kWhite);
@@ -162,7 +164,9 @@ int main(int argc, char** argv) {
             vthr_adc_value_err[i](j) = fun_spec[i][j]->GetParError(0);
             vthr_adc_sigma[i](j) = fun_spec[i][j]->GetParameter(1);
             vthr_adc_sigma_err[i](j) = fun_spec[i][j]->GetParError(1);
+            tri_spec[i][j]->Write();
         }
+        t_file_out->cd();
         canvas_spec[i]->Write();
         vthr_adc_value[i].Write(Form("vthr_adc_value_CT_%02d", i + 1));
         vthr_adc_value_err[i].Write(Form("vthr_adc_value_err_CT_%02d", i + 1));
