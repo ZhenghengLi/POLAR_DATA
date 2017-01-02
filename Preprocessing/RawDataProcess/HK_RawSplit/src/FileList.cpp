fc9dd256f388861ea171c031f419f5645245e7a9
Zhengheng Li <zhenghenge@gmail.com>
2017-01-02 16:39:44 +0800

    add HK_RawSplit

---
+++ Preprocessing/RawDataProcess/HK_RawSplit/src/FileList.cpp
@@ -0,0 +1,235 @@
+#include "FileList.hpp"
+
+using namespace std;
+
+FileList::FileList() {
+
+}
+
+FileList::~FileList() {
+
+}
+
+bool FileList::initialize(const char* listfile) {
+    ifstream infile(listfile, ios::in);
+    if (!infile) {
+        cerr << "Can not open file: " << listfile << endl;
+        return false;
+    }
+    string filename;
+    fileList_.clear();
+    while (true) {
+        getline(infile, filename);
+        if (infile.eof()) {
+            break;
+        }
+        fileList_.push_back(filename);
+    }
+    infile.close();
+    if (fileList_.empty()) {
+        return false;
+    } else {
+        set_start();
+        return true;
+    }
+}
+
+void FileList::clear() {
+    fileList_.clear();
+}
+
+void FileList::add_back(const char* filename) {
+    string str_file(filename);
+    fileList_.push_back(str_file);
+}
+
+
+void FileList::set_start() {
+    fileIter_ = fileList_.begin();
+    start_flag_ = true;
+    buffer_is_first_ = true;
+    vernier_begin_ = 0;
+    vernier_end_ = 0;
+    reach_file_end_ = false;
+    found_start_frame_ = false;
+}
+
+bool FileList::next() {
+    if (fileList_.empty())
+        return false;
+    if (start_flag_)
+        start_flag_ = false;
+    else
+        ++fileIter_;
+    if (fileIter_ == fileList_.end())
+        return false;
+    else
+        return true;
+}
+
+const char* FileList::cur_file() {
+    return (*fileIter_).c_str();
+}
+
+void FileList::shift_left_() {
+    memmove(data_buffer, data_buffer + vernier_begin_, vernier_end_ - vernier_begin_);
+    vernier_end_ -= vernier_begin_;
+    vernier_begin_ = 0;
+}
+
+bool FileList::check_header_() {
+    uint16_t frame_header = 0;
+    for (int i = 0; i < 2; i++) {
+        frame_header <<= 8;
+        frame_header += static_cast<uint8_t>(data_buffer[vernier_begin_ + 0 + i]);
+    }
+    if (frame_header != 0x0749)
+        return false;
+    uint16_t frame_length = 0;
+    for (int i = 0; i < 2; i++) {
+        frame_length <<= 8;
+        frame_length += static_cast<uint8_t>(data_buffer[vernier_begin_ + 4 + i]);
+    }
+    if (frame_length != 0x00F9)
+        return false;
+    uint32_t hk_header = 0;
+    for (int i = 0; i < 4; i++) {
+        hk_header <<= 8;
+        hk_header += static_cast<uint8_t>(data_buffer[vernier_begin_ + 6 + i]);
+    }
+    if (hk_header != 0xD9A4C2EA)
+        return false;
+
+    return true;
+}
+
+bool FileList::next_file() {
+    datafile_.close();
+    if (start_flag_)
+        start_flag_ = false;
+    else
+        ++fileIter_;
+    if (fileIter_ == fileList_.end())
+        return false;
+    // prepare the first data_buffer
+    cout << cur_file() << endl;
+    datafile_.open(cur_file(), ios::in|ios::binary);
+    if (!datafile_.is_open()) {
+        cerr << "Data file open failed: " << cur_file() << endl;
+        return false;
+    }
+    buffer_is_first_ = true;
+    reach_file_end_ = false;
+    found_start_frame_ = false;
+    int check_size = 0;
+    while (!reach_file_end_) {
+        datafile_.read(data_buffer + vernier_end_, BUFFER_SIZE - vernier_end_);
+        vernier_end_ += datafile_.gcount();
+        if (datafile_.eof()) {
+            reach_file_end_ = true;
+            break;
+        }
+        check_size += datafile_.gcount();
+        if (check_size / 260 > CHECK_MAX) {
+            cerr << "INVALID POLAR HK FILE." << endl;
+            return false;
+        }
+        if (check_header_()) {
+            found_start_frame_ = true;
+            while (vernier_begin_ < 259) {
+                vernier_begin_++;
+                if (check_header_()) {
+                    shift_left_();
+                    found_start_frame_ = false;
+                    break;
+                }
+            }
+            vernier_begin_ = 0;
+        } else {
+            vernier_begin_++;
+            shift_left_();
+        }
+        if (found_start_frame_) {
+            break;
+        }
+    }
+    if (found_start_frame_) {
+        return true;
+    } else {
+        cerr << "Could not find the first frame: " << cur_file() << endl;
+        return false;
+    }
+}
+
+bool FileList::next_frame() {
+    if (!found_start_frame_) {
+        vernier_begin_ = 0;
+        vernier_end_ = 0;
+        return false;
+    }
+
+    if (buffer_is_first_) {
+        buffer_is_first_ = false;
+        return true;
+    }
+    if (vernier_end_ - vernier_begin_ < 260)
+        return false;
+    vernier_begin_ += 260;
+    shift_left_();
+    if (vernier_end_ - vernier_begin_ < 260)
+        return false;
+    bool found_header = false;
+    while (!reach_file_end_) {
+        datafile_.read(data_buffer + vernier_end_, BUFFER_SIZE - vernier_end_);
+        vernier_end_ += datafile_.gcount();
+        if (datafile_.eof()) {
+            reach_file_end_ = true;
+            break;
+        }
+        if (check_header_()) {
+            found_header = true;
+            while (vernier_begin_ < 259) {
+                vernier_begin_++;
+                if (check_header_()) {
+                    shift_left_();
+                    found_header = false;
+                    break;
+                }
+            }
+            vernier_begin_ = 0;
+        } else {
+            vernier_begin_++;
+            shift_left_();
+        }
+        if (found_header) {
+            break;
+        }
+    }
+    if (!found_header) {
+        while (vernier_end_ - vernier_begin_ >= 260) {
+            if (check_header_()) {
+                found_header = true;
+                while (vernier_begin_ < 250) {
+                    vernier_begin_++;
+                    if (check_header_()) {
+                        shift_left_();
+                        found_header = false;
+                        break;
+                    }
+                }
+                vernier_begin_ = 0;
+            } else {
+                vernier_begin_++;
+                shift_left_();
+            }
+            if (found_header) {
+                break;
+            }
+        }
+    }
+    if (found_header)
+        return true;
+    else
+        return false;
+}
+
---
+++ Preprocessing/RawDataProcess/HK_RawSplit/src/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdint.h>
+#include <fstream>
+#include <boost/crc.hpp>
+#include "FileList.hpp"
+
+using namespace std;
+using boost::crc_optimal;
+
+int week_of_gps6(const uint64_t raw_gps) {
+    return static_cast<int>((raw_gps >> 32) & 0xFFFF);
+}
+
+double second_of_gps6(const uint64_t raw_gps) {
+    return static_cast<double>((raw_gps >> 12) & 0xFFFFF) + static_cast<double>(raw_gps & 0xFFF) * 0.5 * 1.0E-3;
+}
+
+double read_gps_time(char* frame) {
+    int64_t frm_gps_time_ = 0;
+    for (int i = 0; i < 6; i++) {
+        frm_gps_time_ <<= 8;
+        frm_gps_time_ += static_cast<uint8_t>(frame[250 + i]);
+    }
+    return week_of_gps6(frm_gps_time_) * 604800 + second_of_gps6(frm_gps_time_);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        cout << "USAGE: " << argv[0] << " <rawdata_filename.dat>" << endl;
+        return 2;
+    }
+
+    FileList filelist;
+    filelist.add_back(argv[1]);
+
+    filelist.set_start();
+    if (!filelist.next_file()) {
+        cout << "raw data file open failed" << endl;
+        return 1;
+    }
+
+    crc_optimal<32, 0x04C11DB7> crc_32_;
+
+    ofstream outfile;
+    char filename_buffer[80];
+
+    bool start_flag = true;
+    double pre_gps_time = 0;
+    double cur_gps_time = 0;
+    uint32_t expected, result;
+    int file_id = -1;
+    while (filelist.next_frame()) {
+        // check crc
+        expected = 0;
+        for (int i = 0; i < 4; i++) {
+            expected <<= 8;
+            expected += static_cast<uint8_t>(filelist.data_buffer[256 + i]);
+        }
+        crc_32_.reset();
+        crc_32_.process_bytes(filelist.data_buffer, 256);
+        result = crc_32_.checksum();
+        if (result != expected) continue;
+
+        cur_gps_time = read_gps_time(filelist.data_buffer);
+
+        if (start_flag || cur_gps_time < pre_gps_time) {
+            start_flag = false;
+            file_id++;
+            sprintf(filename_buffer, "hk_segment_%02d.dat", file_id);
+            outfile.close();
+            cout << "open file: " << filename_buffer << " ... " << endl;
+            outfile.open(filename_buffer, ios::out|ios::binary);
+            pre_gps_time = cur_gps_time;
+            outfile.write(filelist.data_buffer, 260);
+        } else {
+            pre_gps_time = cur_gps_time;
+            outfile.write(filelist.data_buffer, 260);
+        }
+
+    }
+
+    return 0;
+}
