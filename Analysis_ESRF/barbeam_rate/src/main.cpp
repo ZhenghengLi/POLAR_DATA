8ed9c4f124d4e9eedc56efe21ae203e3816b71af
Zhengheng Li <zhenghenge@gmail.com>
2017-01-14 15:33:49 +0800

    finish barbeam_rate

--- Analysis_ESRF/barbeam_rate/src/main.cpp
+++ Analysis_ESRF/barbeam_rate/src/main.cpp
@@ -52,7 +52,87 @@ int main(int argc, char** argv) {
     t_event_tree->SetBranchStatus("trigger_bit", true);
     t_event_tree->SetBranchStatus("current", true);
 
+    // read barbeam time span
+    TFile* t_rate_file = new TFile(rate_fn.c_str(), "read");
+    TMatrixF* tmp_mat;
+    TMatrixF begin_time_mat(25, 64), end_time_mat(25, 64);
+    tmp_mat = static_cast<TMatrixF*>(t_rate_file->Get("begin_time_mat"));
+    if (tmp_mat != NULL) {
+        begin_time_mat = *tmp_mat;
+    } else {
+        cout << "cannot find TMatrixF begin_time_mat" << endl;
+        return 1;
+    }
+    tmp_mat = static_cast<TMatrixF*>(t_rate_file->Get("end_time_mat"));
+    if (tmp_mat != NULL) {
+        end_time_mat = *tmp_mat;
+    } else {
+        cout << "cannot find TMatrixF end_tim_mat" << endl;
+        return 1;
+    }
+    t_rate_file->Close();
+    delete t_rate_file;
+    t_rate_file = NULL;
 
+    // open output file
+    TFile* t_file_out = new TFile(output_fn.c_str(), "recreate");
+
+    int    barbeam_total_counts[25][64];
+    double barbeam_total_intensity[25][64];
+    // initialize
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 64; j++) {
+            barbeam_total_counts[i][j] = 0;
+            barbeam_total_intensity[i][j] = 0;
+        }
+    }
+
+    // read data
+    int pre_percent = 0;
+    int cur_percent = 0;
+    cout << "reading data ..." << endl;
+    cout << "[ " << flush;
+    for (Long64_t q = 0; q < t_event_tree->GetEntries(); q++) {
+        cur_percent = static_cast<int>(q * 100.0 / t_event_tree->GetEntries());
+        if (cur_percent - pre_percent > 0 && cur_percent % 2 == 0) {
+            pre_percent = cur_percent;
+            cout << "#" << flush;
+        }
+        t_event_tree->GetEntry(q);
+        for (int i = 0; i < 25; i++) {
+            if (!t_event.time_aligned[i]) continue;
+            for (int j = 0; j < 64; j++) {
+                if (t_event.trigger_bit[i][j] && t_event.ct_time_second > begin_time_mat[i][j] && t_event.ct_time_second < end_time_mat[i][j]) {
+                    barbeam_total_counts[i][j] += 1;
+                    barbeam_total_intensity[i][j] += t_event.current;
+                }
+            }
+        }
+
+    }
+    cout << " DONE ]" << endl;
+    t_event_file->Close();
+    t_beam_intensity_file->Close();
+
+    // calculate intensity and rate of each bar
+    TMatrixF bar_rate(25, 64);
+    TMatrixF bar_intensity(25, 64);
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 64; j++) {
+            if (end_time_mat(i, j) - begin_time_mat(i, j) > 0 && barbeam_total_counts[i][j] > 0) {
+                bar_rate(i, j) = barbeam_total_counts[i][j] / (end_time_mat(i, j) - begin_time_mat(i, j));
+                bar_intensity(i, j) = barbeam_total_intensity[i][j] / barbeam_total_counts[i][j];
+            } else {
+                bar_rate(i, j) = -1;
+                bar_intensity(i, j) = -1;
+            }
+        }
+    }
+
+    t_file_out->cd();
+    bar_rate.Write("bar_rate");
+    bar_intensity.Write("bar_intensity");
+    t_file_out->Close();
 
     return 0;
 }
