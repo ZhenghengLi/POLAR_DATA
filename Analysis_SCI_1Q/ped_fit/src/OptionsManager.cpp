c679c8275a7cb03427c94adcb7d7752e2392d31f
Zhengheng Li <zhenghenge@gmail.com>
2017-04-10 23:10:06 +0800

    delete noise_fit, start ped_fit

---
+++ Analysis_SCI_1Q/ped_fit/src/OptionsManager.cpp
@@ -0,0 +1,133 @@
+#include "OptionsManager.hpp"
+
+using namespace std;
+
+OptionsManager::OptionsManager() {
+    init();
+}
+
+OptionsManager::~OptionsManager() {
+
+}
+
+bool OptionsManager::parse(int argc_par, char** argv_par) {
+    if (argc_par < 2)
+        return false;
+    TString cur_par_str;
+    int idx = 0;
+    while (idx < argc_par - 1) {
+        cur_par_str = argv_par[++idx];
+        if (cur_par_str == "--version") {
+            version_flag_ = true;
+            return false;
+        }
+        if (cur_par_str[0] == '-') {
+            if (cur_par_str.Length() != 2)
+                return false;
+            char cur_option = cur_par_str[1];
+            switch (cur_option) {
+            case 'o':
+                if (idx < argc_par - 1) {
+                    ped_result_filename = argv_par[++idx];
+                    if (ped_result_filename[0] == '-') {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+                break;
+            case 'n':
+                if (idx < argc_par - 1) {
+                    TString tmp_arg = argv_par[++idx];
+                    if (tmp_arg[0] == '-') {
+                        return false;
+                    } else {
+                        ct_num = tmp_arg.Atoi();
+                    }
+                } else {
+                    return false;
+                }
+                break;
+            case 'a':
+                if (idx < argc_par - 1) {
+                    TString tmp_arg = argv_par[++idx];
+                    if (tmp_arg[0] == '-') {
+                        return false;
+                    } else {
+                        low_temp = tmp_arg.Atof();
+                    }
+                } else {
+                    return false;
+                }
+                break;
+            case 'b':
+                if (idx < argc_par - 1) {
+                    TString tmp_arg = argv_par[++idx];
+                    if (tmp_arg[0] == '-') {
+                        return false;
+                    } else {
+                        high_temp = tmp_arg.Atof();
+                    }
+                } else {
+                    return false;
+                }
+                break;
+            default:
+                return false;
+            }
+        } else {
+            if (ped_data_filename.IsNull()) {
+                ped_data_filename = cur_par_str;
+            } else {
+                return false;
+            }
+        }
+    }
+    if (ped_data_filename.IsNull()) return false;
+    if (ct_num < 1 || ct_num > 25) return false;
+    if (low_temp > high_temp) return false;
+    if (ped_result_filename.IsNull()) {
+        ped_result_filename = "ped_result.root";
+    }
+    return true;
+}
+
+void OptionsManager::print_help() {
+    cout << "Usage:" << endl;
+    cout << "  " << SW_NAME << "<ped_data.root> -n <ct_num> [-a <low_temp>] [-b <high_temp>] [-o <ped_res.root>]" << endl;
+    cout << endl;
+    cout << "Options:" << endl;
+    cout << "  -n <ct_num>                      select which module to process" << endl;
+    cout << "  -a <low_temp>                    low temperature" << endl;
+    cout << "  -b <high_temp>                   high temperature" << endl;
+    cout << "  -o <ped_res.root>                root file to store pedestal fitting result" << endl;
+    cout << endl;
+    cout << "  --version                        print version and author information" << endl;
+    cout << endl;
+}
+
+void OptionsManager::print_version() {
+    cout << endl;
+    cout << "    " << SW_NAME << " - POLAR 1Q Level Data Analysis Chain" << endl;
+    cout << "    " << SW_VERSION << " (" << RELEASE_DATE << ", compiled " << __DATE__ << " " << __TIME__ << ")" << endl;
+    cout << endl;
+    cout << " Copyright (C) 2015-2016 Zhengheng Li <lizhengheng@ihep.ac.cn>" << endl;
+    cout << endl;
+}
+
+void OptionsManager::print_options() {
+
+}
+
+void OptionsManager::init() {
+    ped_data_filename.Clear();
+    ct_num = -1;
+    low_temp = 127;
+    high_temp = 128;
+
+    version_flag_ = false;
+}
+
+bool OptionsManager::get_version_flag() {
+    return version_flag_;
+}
--- Analysis_SCI_1Q/ped_fit/src/main.cpp
+++ Analysis_SCI_1Q/ped_fit/src/main.cpp
@@ -1,8 +1,95 @@
 #include <iostream>
+#include "RootInc.hpp"
+#include "OptionsManager.hpp"
+#include "CooConv.hpp"
+
+#define PED_BINS 256
+#define PED_MAX  1024
 
 using namespace std;
 
 int main(int argc, char** argv) {
+    OptionsManager options_mgr;
+    if (!options_mgr.parse(argc, argv)) {
+        if (options_mgr.get_version_flag()) {
+            options_mgr.print_version();
+        } else {
+            options_mgr.print_help();
+        }
+        return 2;
+    }
+
+    // open ped_data_file
+    TFile* ped_data_file = new TFile(options_mgr.ped_data_filename.Data(), "read");
+    if (ped_data_file->IsZombie()) {
+        cout << "ped_data_file open failed." << endl;
+        return 1;
+    }
+    TTree* t_ped_data_tree = static_cast<TTree*>(ped_data_file->Get(Form("t_ped_data_ct_%02d", options_mgr.ct_num)));
+    if (t_ped_data_tree == NULL) {
+        cout << "cannot find TTree " << Form("t_ped_data_ct_%02d", options_mgr.ct_num) << endl;
+        return 1;
+    }
+    struct {
+        Double_t event_time;
+        Bool_t   trigger_bit[64];
+        Float_t  ped_adc[64];
+        Float_t  fe_temp;
+        Float_t  aux_interval;
+    } t_ped_data;
+    t_ped_data_tree->SetBranchAddress("event_time",     &t_ped_data.event_time     );
+    t_ped_data_tree->SetBranchAddress("trigger_bit",     t_ped_data.trigger_bit    );
+    t_ped_data_tree->SetBranchAddress("ped_adc",         t_ped_data.ped_adc        );
+    t_ped_data_tree->SetBranchAddress("fe_temp",        &t_ped_data.fe_temp        );
+    t_ped_data_tree->SetBranchAddress("aux_interval",   &t_ped_data.aux_interval   );
+    cout << options_mgr.ped_data_filename.Data() << " { " << "ct_num = " << options_mgr.ct_num << " }" << endl;
+
+    // open output file
+    TFile* ped_result_file = new TFile(options_mgr.ped_result_filename.Data(), "recreate");
+    if (ped_result_file->IsZombie()) {
+        cout << "ped_result_file open failed." << endl;
+        return 1;
+    }
+
+    // prepare histogram
+    TH1D* ped_hist[64];
+    for (int j = 0; j < 64; j++) {
+        ped_hist[j] = new TH1D(Form("ped_hist_%02d_%02d", options_mgr.ct_num, j + 1),
+                Form("Pedestal of CH %02d_%02d", options_mgr.ct_num, j + 1),
+                PED_BINS, 0, PED_MAX);
+        ped_hist[j]->SetDirectory(ped_result_file);
+    }
+    TH1D* common_noise_hist;
+    common_noise_hist = new TH1D(Form("common_noise_%02d", options_mgr.ct_num),
+            Form("Common noise of Module CT_%02d", options_mgr.ct_num), 256, -128, 128);
+    common_noise_hist->SetDirectory(ped_result_file);
+    TH1D* ped_shift_hist[64];
+    for (int j = 0; j < 64; j++) {
+        ped_shift_hist[j] = new TH1D(Form("ped_shift_hist_%02d_%02d", options_mgr.ct_num, j + 1),
+                Form("Pedestal shift of CH %02d_%02d", options_mgr.ct_num, j + 1),
+                256, -128, 128);
+        ped_shift_hist[j]->SetDirectory(ped_result_file);
+    }
+    TVectorF ped_mean_vec(64);
+    TVectorF ped_sigma_vec(64);
+    TVectorF ped_shift_mean_vec(64);
+    TVectorF ped_shift_sigma_vec(64);
+    Float_t  common_noise;
+
+    // reading and selecting data
+    int pre_percent = 0;
+    int cur_percent = 0;
+    cout << "reading and selecting data ... " << endl;
+    cout << "[ " << flush;
+    for (Long64_t q = 0; q < t_ped_data_tree->GetEntries(); q++) {
+        cur_percent = static_cast<int>(q * 100.0 / t_ped_data_tree->GetEntries());
+        if (cur_percent - pre_percent > 0 && cur_percent % 2 == 0) {
+            pre_percent = cur_percent;
+            cout << "#" << flush;
+        }
+        t_ped_data_tree->GetEntry(q);
+
+    }
 
     return 0;
 }
