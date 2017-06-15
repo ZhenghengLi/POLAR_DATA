f130a548a6aef2b89d9478eaf7e860fcade6aff4
Zhengheng Li <zhenghenge@gmail.com>
2017-06-15 21:07:35 +0800

    finish energy_recon

---
+++ Analysis_SCI_1Q/energy_recon/src/Gain.cpp
@@ -0,0 +1,99 @@
+#include "Gain.hpp"
+
+using namespace std;
+
+Gain::Gain() {
+    gain_vs_hv_p0_mat_.ResizeTo(25, 64);
+    gain_vs_hv_p1_mat_.ResizeTo(25, 64);
+    bad_calib_mat.ResizeTo(25, 64);
+    for (int i = 0; i < 25; i++) {
+        adc_per_kev_CT_[i].ResizeTo(64);
+        gain_vec_CT[i].ResizeTo(64);
+    }
+}
+
+Gain::~Gain() {
+
+}
+
+bool Gain::read_gain_vec(const char* gain_vec_filename) {
+    TFile* gain_vec_file = new TFile(gain_vec_filename, "read");
+    if (gain_vec_file->IsZombie()) {
+        cout << "gain_vec_file open failed: " << gain_vec_filename << endl;
+        return false;
+    }
+    TMatrixF* tmp_mat;
+    tmp_mat = static_cast<TMatrixF*>(gain_vec_file->Get("bad_calib_mat"));
+    if (tmp_mat == NULL) {
+        cout << "cannot find TMatrixF bad_calib_mat." << endl;
+        return false;
+    } else {
+        bad_calib_mat = *tmp_mat;
+    }
+    TVectorF* tmp_vec;
+    for (int i = 0; i < 25; i++) {
+        tmp_vec = static_cast<TVectorF*>(gain_vec_file->Get(Form("adc_per_kev_ct_%02d", i + 1)));
+        if (tmp_vec == NULL) {
+            cout << "cannot find TVectorF " << Form("adc_per_kev_ct_%02d", i + 1) << endl;
+            return false;
+        } else {
+            adc_per_kev_CT_[i] = *tmp_vec;
+        }
+    }
+    gain_vec_file->Close();
+    delete gain_vec_file;
+    gain_vec_file = NULL;
+
+    return true;
+}
+
+bool Gain::read_gain_vs_hv(const char* gain_vs_hv_filename) {
+    TFile* gain_vs_hv_file = new TFile(gain_vs_hv_filename, "read");
+    if (gain_vs_hv_file->IsZombie()) {
+        cout << "gain_vs_hv_file open failed: " << gain_vs_hv_filename << endl;
+        return false;
+    }
+    TMatrixF* tmp_mat;
+    tmp_mat = static_cast<TMatrixF*>(gain_vs_hv_file->Get("bad_calib_mat"));
+    if (tmp_mat == NULL) {
+        cout << "cannot find TMatrixF bad_calib_mat." << endl;
+        return false;
+    } else {
+        bad_calib_mat = *tmp_mat;
+    }
+    tmp_mat = static_cast<TMatrixF*>(gain_vs_hv_file->Get("gain_vs_hv_p0_mat"));
+    if (tmp_mat == NULL) {
+        cout << "cannot find TMatrixF gain_vs_hv_p0_mat." << endl;
+        return false;
+    } else {
+        gain_vs_hv_p0_mat_ = *tmp_mat;
+    }
+    tmp_mat = static_cast<TMatrixF*>(gain_vs_hv_file->Get("gain_vs_hv_p1_mat"));
+    if (tmp_mat == NULL) {
+        cout << "cannot find TMatrixF gain_vs_hv_p1_mat." << endl;
+        return false;
+    } else {
+        gain_vs_hv_p1_mat_ = *tmp_mat;
+    }
+    gain_vs_hv_file->Close();
+    delete gain_vs_hv_file;
+    gain_vs_hv_file = NULL;
+
+    return true;
+}
+
+void Gain::gen_gain() {
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 64; j++) {
+            gain_vec_CT[i](j) = adc_per_kev_CT_[i](j);
+        }
+    }
+}
+
+void Gain::gen_gain(int ct_num, double hv) {
+    int ct_idx = ct_num - 1;
+    for (int j = 0; j < 64; j++) {
+        gain_vec_CT[ct_idx](j) = TMath::Exp(gain_vs_hv_p0_mat_(ct_idx, j) + gain_vs_hv_p1_mat_(ct_idx, j) * TMath::Log(hv));
+    }
+}
+
--- Analysis_SCI_1Q/energy_recon/src/main.cpp
+++ Analysis_SCI_1Q/energy_recon/src/main.cpp
@@ -1,8 +1,134 @@
 #include <iostream>
+#include <algorithm>
+#include "OptionsManager.hpp"
+#include "RootInc.hpp"
+#include "POLEvent.hpp"
+#include "Gain.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv) {
+    OptionsManager options_mgr;
+    if (!options_mgr.parse(argc, argv)) {
+        if (options_mgr.get_version_flag()) {
+            options_mgr.print_version();
+        } else {
+            options_mgr.print_help();
+        }
+        return 2;
+    }
+
+    // prepare gain
+    Gain gain_obj;
+    if (options_mgr.gain_hv_flag) {
+        if (!gain_obj.read_gain_vs_hv(options_mgr.gain_vs_hv_filename.Data())) {
+            cout << "gain_vs_hv read failed: " << options_mgr.gain_vs_hv_filename.Data() << endl;
+            return 1;
+        }
+    } else {
+        if (!gain_obj.read_gain_vec(options_mgr.gain_vec_filename.Data())) {
+            cout << "gain_vec read failed: " << options_mgr.gain_vec_filename.Data() << endl;
+            return 1;
+        } else {
+            gain_obj.gen_gain();
+        }
+    }
+
+    // read event data
+    TFile* pol_event_file = new TFile(options_mgr.pol_event_filename.Data(), "read");
+    if (pol_event_file->IsZombie()) {
+        cout << "pol_event_file open failed: " << options_mgr.pol_event_filename.Data() << endl;
+        return 1;
+    }
+    TNamed* m_level_num = static_cast<TNamed*>(pol_event_file->Get("m_level_num"));
+    if (m_level_num != NULL) {
+        if (TString(m_level_num->GetTitle()).Atoi() != 2) {
+            cout << "m_level_num is not 2, need to do pedestal & common noise subtraction and crosstalk correction." << endl;
+            return 1;
+        }
+    }
+    TTree* t_pol_event_tree = static_cast<TTree*>(pol_event_file->Get("t_pol_event"));
+    if (t_pol_event_tree == NULL) {
+        cout << "cannot find TTree t_pol_event." << endl;
+        return 1;
+    }
+    POLEvent t_pol_event;
+    t_pol_event.bind_pol_event_tree(t_pol_event_tree);
+
+    // open output file
+    TFile* pol_event_file_energy = new TFile(options_mgr.output_filename.Data(), "recreate");
+    if (pol_event_file_energy->IsZombie()) {
+        cout << "pol_event_file_energy open failed: " << options_mgr.output_filename.Data() << endl;
+        return 1;
+    }
+    TTree* t_pol_event_tree_new = t_pol_event_tree->CloneTree(0);
+
+    TVectorF energy_value_vector;
+    energy_value_vector.ResizeTo(64);
+
+    // do energy reconstruction
+    int pre_percent = 0;
+    int cur_percent = 0;
+    cout << "Doing energy reconstructing ... " << endl;
+    cout << "[ " << flush;
+    for (Long64_t q = 0; q < t_pol_event_tree->GetEntries(); q++) {
+        cur_percent = static_cast<int>(q * 100.0 / t_pol_event_tree->GetEntries());
+        if (cur_percent - pre_percent > 0 && cur_percent % 2 == 0) {
+            pre_percent = cur_percent;
+            cout << "#" << flush;
+        }
+        t_pol_event_tree->GetEntry(q);
+
+        // (1) convert ADC to keV
+        for (int i = 0; i < 25; i++) {
+            if (t_pol_event.time_aligned[i]) {
+                if (options_mgr.gain_hv_flag) {
+                    gain_obj.gen_gain(i + 1, t_pol_event.fe_hv[i]);
+                }
+                for (int j = 0; j < 64; j++) {
+                    if (gain_obj.bad_calib_mat(i, j)) {
+                        t_pol_event.channel_status[i][j] += POLEvent::BAD_CALIB;
+                    }
+                    t_pol_event.energy_value[i][j] /= gain_obj.gain_vec_CT[i](j);
+                }
+            }
+        }
+        // (2) store energy reconstructed data
+        t_pol_event_tree_new->Fill();
+    }
+    cout << " DONE ]" << endl;
+
+    // write TTree
+    pol_event_file_energy->cd();
+    t_pol_event_tree_new->Write();
+
+    cout << "writing meta information ... " << endl;
+    // write meta
+    TIter fileIter(pol_event_file->GetListOfKeys());
+    TKey* key = NULL;
+    TNamed* meta = NULL;
+    while ((key = static_cast<TKey*>(fileIter.Next())) != NULL) {
+        if (string(key->GetClassName()) != "TNamed") continue;
+        if (string(key->GetName()) == "m_energy_unit") {
+            meta = static_cast<TNamed*>(key->ReadObj());
+            meta->SetTitle("keV");
+            meta->Write();
+        } else if (string(key->GetName()) == "m_level_num") {
+            meta = static_cast<TNamed*>(key->ReadObj());
+            meta->SetTitle("3");
+            meta->Write();
+        } else {
+            meta = static_cast<TNamed*>(key->ReadObj());
+            meta->Write();
+        }
+    }
+
+    // close output file
+    pol_event_file_energy->Close();
+    delete pol_event_file_energy;
+    pol_event_file_energy = NULL;
+
+    cout << "[ DONE ]" << endl;
 
     return 0;
 }
