684c109ce3e38bfb5c0de8d527f3da1da179ec8f
Zhengheng Li <zhenghenge@gmail.com>
2017-07-03 17:12:32 +0800

    update theta_pha_calc.py

--- Tools/script/theta_pha_calc.py
+++ Tools/script/theta_pha_calc.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python
 
-det_x_ra  = 3.6289
-det_x_dec = 36.715
-det_z_ra  = 18.070
-det_z_dec = 47.103
-sun_ra    = 12.145
-sun_dec   = -0.9439
+import argparse
+
+parser = argparse.ArgumentParser(description='calculate theta and pha of GRB')
+parser.add_argument('grb_radec', help = '"RA(deg),DEC(deg)" of GRB')
+parser.add_argument('-Z', dest = 'z_radec', required=True, help = '"RA(h),DEC(deg) of POLAR Z axis"')
+parser.add_argument('-X', dest = 'x_radec', required=True, help = '"RA(h),DEC(deg) of POLAR X axis"')
+args = parser.parse_args()
+
+print args.grb_radec
+print args.z_radec
+print args.x_radec
+
+grb_radec_vec = [float(x) for x in args.grb_radec.split(',')]
+z_radec_vec   = [float(x) for x in args.z_radec.split(',')]
+x_radec_vec   = [float(x) for x in args.x_radec.split(',')]
+
+if len(grb_radec_vec) != 2 or len(z_radec_vec) != 2 or len(x_radec_vec) != 2:
+    print "Wrong RA/DEC input"
+    exit(1)
+
+det_x_ra, det_x_dec = x_radec_vec
+det_z_ra, det_z_dec = z_radec_vec
+grb_ra    = grb_radec_vec[0] / 360.0 * 24.0
+grb_dec   = grb_radec_vec[1]
 
 import numpy as np
 
@@ -26,7 +44,7 @@ def normalize(xyz):
 vec_x = radec_to_vec(det_x_ra, det_x_dec)
 vec_z = radec_to_vec(det_z_ra, det_z_dec)
 vec_y = np.cross(vec_z, vec_x)
-vec_s = normalize(radec_to_vec(sun_ra, sun_dec))
+vec_s = normalize(radec_to_vec(grb_ra, grb_dec))
 
 cos_theta = np.dot(vec_s, vec_z)
 pha_x = np.dot(vec_s, vec_x)
