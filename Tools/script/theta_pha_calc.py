d9d913388ac701cc656e7ee108b08d212e44915b
Zhengheng Li <zhenghenge@gmail.com>
2017-07-03 17:22:32 +0800

    update of theta_pha_calc.py

--- Tools/script/theta_pha_calc.py
+++ Tools/script/theta_pha_calc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+import numpy as np
 
 parser = argparse.ArgumentParser(description='calculate theta and pha of GRB')
 parser.add_argument('grb_radec', help = '"RA(deg),DEC(deg)" of GRB')
@@ -20,8 +21,6 @@ det_x_ra, det_x_dec = x_radec_vec
 det_z_ra, det_z_dec = z_radec_vec
 grb_ra    = grb_radec_vec[0] / 360.0 * 24.0
 grb_dec   = grb_radec_vec[1]
-
-import numpy as np
 
 def deg_to_rad(deg):
     return deg / 180.0 * np.pi
@@ -51,3 +50,4 @@ pha   = rad_to_deg(np.arctan2(pha_y, pha_x))
 
 print "theta: " + str(theta)
 print "pha:   " + str(pha)
+
