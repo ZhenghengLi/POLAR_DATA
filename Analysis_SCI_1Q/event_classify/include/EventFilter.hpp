b20ec605ae4bcf8d00044bafc5fac1031e56f171
Zhengheng Li <zhenghenge@gmail.com>
2017-09-28 13:55:42 +0800

    event_classify: add too_short check

--- Analysis_SCI_1Q/event_classify/include/EventFilter.hpp
+++ Analysis_SCI_1Q/event_classify/include/EventFilter.hpp
@@ -9,17 +9,25 @@ using namespace std;
 
 class EventFilter {
 public:
-    static const UShort_t TOO_LOW  = 0x1;
-    static const UShort_t TOO_MANY = 0x2;
+    static const UShort_t TOO_LOW   = 0x1;
+    static const UShort_t TOO_MANY  = 0x2;
+    static const UShort_t TOO_SHORT = 0x4;
 
 private:
-    static const UShort_t too_low_cut_ = 100.0;
-    static const UShort_t too_many_cut_1_ = 15;
-    static const UShort_t too_many_cut_2_ = 35;
+    static const double too_low_cut_;
+    static const int    too_many_cut_1_;
+    static const int    too_many_cut_2_;
+    static const double too_short_cut_;
+
+private:
+    double bar_timewait_cache_[4][25][64];
+    bool   bar_trigger_first_[25][64];
+    double bar_pre_event_time_[25][64];
 
 private:
     UShort_t check_too_low_(const POLEvent& pol_event);
     UShort_t check_too_many_(const POLEvent& pol_event);
+    UShort_t check_too_short_(const POLEvent& pol_event);
 
 public:
     EventFilter();
--- Analysis_SCI_1Q/event_classify/src/EventFilter.cpp
+++ Analysis_SCI_1Q/event_classify/src/EventFilter.cpp
@@ -1,7 +1,21 @@
 #include "EventFilter.hpp"
 
-EventFilter::EventFilter() {
+// intialize cut constant
+const double EventFilter::too_low_cut_    = 100.0;
+const int    EventFilter::too_many_cut_1_ = 15;
+const int    EventFilter::too_many_cut_2_ = 35;
+const double EventFilter::too_short_cut_  = 0.01;
 
+EventFilter::EventFilter() {
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 64; j++) {
+            for (int k = 0; k < 4; k++) {
+                bar_timewait_cache_[k][i][j] = 10.0;
+            }
+            bar_trigger_first_[i][j] = true;
+            bar_pre_event_time_[i][j] = 0.0;
+        }
+    }
 }
 
 EventFilter::~EventFilter() {
@@ -54,13 +68,42 @@ UShort_t EventFilter::check_too_many_(const POLEvent& pol_event) {
     return 0;
 }
 
+UShort_t EventFilter::check_too_short_(const POLEvent& pol_event) {
+    for (int i = 0; i < 25; i++) {
+        if (!pol_event.time_aligned[i]) continue;
+        for (int j = 0; j < 64; j++) {
+            if (pol_event.trigger_bit[i][j]) {
+                if (bar_trigger_first_[i][j]) {
+                    bar_trigger_first_[i][j] = false;
+                    bar_pre_event_time_[i][j] = pol_event.event_time;
+                    continue;
+                }
+                for (int k = 0; k < 3; k++) {
+                    bar_timewait_cache_[k][i][j] = bar_timewait_cache_[k + 1][i][j];
+                }
+                bar_timewait_cache_[3][i][j] = pol_event.event_time - bar_pre_event_time_[i][j];
+                bar_pre_event_time_[i][j] = pol_event.event_time;
+                if (bar_timewait_cache_[0][i][j] < too_short_cut_ &&
+                    bar_timewait_cache_[1][i][j] < too_short_cut_ &&
+                    bar_timewait_cache_[2][i][j] < too_short_cut_ &&
+                    bar_timewait_cache_[3][i][j] < too_short_cut_) {
+                    return TOO_SHORT;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
 UShort_t EventFilter::classify(const POLEvent& pol_event) {
     if (pol_event.is_ped) return 0;
 
     UShort_t too_low_res = check_too_low_(pol_event);
     UShort_t too_many_res = check_too_many_(pol_event);
+    UShort_t too_short_res = check_too_short_(pol_event);
 
-    UShort_t final_res = (too_low_res | too_many_res);
+    UShort_t final_res = (too_low_res | too_many_res | too_short_res);
 
     return final_res;
 }
