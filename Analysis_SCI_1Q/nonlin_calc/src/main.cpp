f858510594b630afc5c480f49f4756e8c4c4c996
Zhengheng Li <zhenghenge@gmail.com>
2017-12-06 17:05:03 +0800

    nonlin_calc: finish code to get the nonlinarity curve

--- Analysis_SCI_1Q/nonlin_calc/src/main.cpp
+++ Analysis_SCI_1Q/nonlin_calc/src/main.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-#define BIN_SIZE 4
-
 int main(int argc, char** argv) {
     OptionsManager options_mgr;
     if (!options_mgr.parse(argc, argv)) {
@@ -18,17 +16,28 @@ int main(int argc, char** argv) {
         return 2;
     }
 
+    const int bin_size_x = 64;
+    const int max_adc_x = 4096;
+    const int bin_size_y = 4;
+    const int max_adc_y = 768;
+
+    int nbins_x = max_adc_x / bin_size_x;
+    int nbins_y = max_adc_y / bin_size_y;
+
     // open output file
     TFile* output_file = new TFile(options_mgr.output_filename.Data(), "recreate");
     if (output_file->IsZombie()) {
         cout << "output_file open failed: " << options_mgr.output_filename << endl;
         return 1;
     }
-    TH2F* max_ADC_spec[25][64];
+    TH2F* max_ADC_spec_tri[25][64];
+    TH2F* max_ADC_spec_all[25][64];
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j <64; j++) {
-            max_ADC_spec[i][j] = new TH2F(Form("max_ADC_spec_%02d_%02d", i + 1, j), Form("max_ADC_spec_%02d_%02d", i + 1, j),
-                    768 / BIN_SIZE, 0, 768, 4096 / BIN_SIZE, 0, 4096);
+            max_ADC_spec_tri[i][j] = new TH2F(Form("max_ADC_spec_tri_%02d_%02d", i + 1, j), Form("max_ADC_spec_tri_%02d_%02d", i + 1, j),
+                    nbins_x, 0, max_adc_x, nbins_y, 0, max_adc_y);
+            max_ADC_spec_all[i][j] = new TH2F(Form("max_ADC_spec_all_%02d_%02d", i + 1, j), Form("max_ADC_spec_all_%02d_%02d", i + 1, j),
+                    nbins_x, 0, max_adc_x, nbins_y, 0, max_adc_y);
         }
     }
 
@@ -85,8 +94,10 @@ int main(int argc, char** argv) {
                 if (!t_pol_event.trigger_bit[i][cur_max_j]) continue;
                 for (int j = 0; j < 64; j++) {
                     if (t_pol_event.channel_status[i][j] > 0) continue;
-                    if (j != cur_max_j && t_pol_event.trigger_bit[i][j]) {
-                        max_ADC_spec[i][j]->Fill(t_pol_event.energy_value[i][j], cur_max_ADC);
+                    if (j == cur_max_j) continue;
+                    max_ADC_spec_all[i][j]->Fill(cur_max_ADC, t_pol_event.energy_value[i][j]);
+                    if (t_pol_event.trigger_bit[i][j]) {
+                        max_ADC_spec_tri[i][j]->Fill(cur_max_ADC, t_pol_event.energy_value[i][j]);
                     }
                 }
             }
@@ -99,16 +110,92 @@ int main(int argc, char** argv) {
 
     }
 
-    // save result
-    gROOT->SetBatch(kTRUE);
-    gStyle->SetOptStat(11);
-    gStyle->SetOptFit(111);
+    cout << "calculating cutoff and saving result ..." << endl;
 
+    gROOT->SetBatch(kTRUE);
+
+    // calculate left cutoff
+    TH1F* h_spec_tri = new TH1F("h_spec_tri", "h_spec_tri", max_adc_y / bin_size_y, 0, max_adc_y);
+    h_spec_tri->SetDirectory(NULL);
+    TH1F* h_spec_all = new TH1F("h_spec_all", "h_spec_all", max_adc_y / bin_size_y, 0, max_adc_y);
+    h_spec_all->SetDirectory(NULL);
+    int tri_stat = 0;
+    TF1* f_ratio = new TF1("f_ratio", "(TMath::Erf((x - [0]) / TMath::Sqrt(2) / [1]) + 1.0) / 2.0", 0, max_adc_y);
+    TGraphErrors* nonlin_curve[25][64];
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 64; j++) {
+            nonlin_curve[i][j] = new TGraphErrors();
+            nonlin_curve[i][j]->SetName(Form("nonlin_curve_%02d_%02d", i + 1, j));
+            nonlin_curve[i][j]->SetTitle(Form("nonlin_curve_%02d_%02d", i + 1, j));
+            for (int k = 1; k <= nbins_x; k++) {
+                tri_stat = 0;
+                // read counts
+                for (int n = 1; n <= nbins_y; n++) {
+                    int binc_tri = max_ADC_spec_tri[i][j]->GetBinContent(k, n);
+                    int binc_all = max_ADC_spec_all[i][j]->GetBinContent(k, n);
+                    tri_stat += binc_tri;
+                    h_spec_tri->SetBinContent(n, binc_tri);
+                    h_spec_all->SetBinContent(n, binc_all);
+                }
+                if (tri_stat < 30) continue;
+                h_spec_tri->Divide(h_spec_all);
+                // find first bin with ratio 1
+                int first_bin_1 = 0;
+                for (int n = 1; n <= nbins_y; n++) {
+                    if (h_spec_tri->GetBinContent(n) > 0.90) {
+                        first_bin_1 = n;
+                        break;
+                    }
+                }
+                // find last bin with ratio 1
+                int last_bin_1 = 0;
+                for (int n = nbins_y; n >= 1; n--) {
+                    if (h_spec_tri->GetBinContent(n) > 0.90) {
+                        last_bin_1 = n;
+                        break;
+                    }
+                }
+                if (last_bin_1 - first_bin_1 < 5) continue;
+                double adc_left = (first_bin_1 - 0.5) * bin_size_y;
+                double adc_right = (last_bin_1 - 0.5) * bin_size_y;
+                f_ratio->SetRange(adc_left - 40, adc_right);
+                f_ratio->SetParameters(adc_left, 15);
+                h_spec_tri->Fit(f_ratio, "RQ");
+                double cutoff_pos = f_ratio->GetParameter(0);
+                double cutoff_err = f_ratio->GetParError(0);
+                double maxADC = (k - 0.5) * bin_size_x;
+                nonlin_curve[i][j]->SetPoint(k, maxADC, cutoff_pos);
+                nonlin_curve[i][j]->SetPointError(k, 0.5 * bin_size_x, cutoff_err);
+            }
+
+        }
+    }
+
+
+    // save result
+    TCanvas* canvas_non_lin[25];
     output_file->cd();
     for (int i = 0; i < 25; i++) {
+        output_file->cd();
+        canvas_non_lin[i] = new TCanvas(Form("canvas_non_lin_%02d", i + 1), Form("canvas_non_lin_%02d", i + 1), 1800, 1500);
+        canvas_non_lin[i]->Divide(8, 8);
+        canvas_non_lin[i]->SetFillColor(kYellow);
+        for (int j = 0; j < 64; j++) {
+            canvas_non_lin[i]->cd(jtoc(j));
+            canvas_non_lin[i]->GetPad(jtoc(j))->SetFillColor(kWhite);
+            canvas_non_lin[i]->GetPad(jtoc(j))->SetLogz();
+            max_ADC_spec_tri[i][j]->Draw("colz");
+            nonlin_curve[i][j]->SetLineColor(kBlue);
+            nonlin_curve[i][j]->SetLineWidth(2);
+            nonlin_curve[i][j]->SetMarkerColor(kRed);
+            nonlin_curve[i][j]->SetMarkerStyle(5);
+            nonlin_curve[i][j]->Draw("same LP");
+        }
+        canvas_non_lin[i]->Write();
         output_file->mkdir(Form("non_linearity_%02d", i + 1))->cd();
         for (int j = 0; j < 64; j++) {
-            max_ADC_spec[i][j]->Write();
+            max_ADC_spec_tri[i][j]->Write();
+            nonlin_curve[i][j]->Write();
         }
     }
     output_file->Close();
