ce7a062f6522f733f54e96042cd682c8e5760204
Zhengheng Li <zhenghenge@gmail.com>
2018-03-27 16:36:06 +0800

    modcur_contour_gen: can work

--- Analysis_SCI_1Q/modcur_contour_gen/src/main.cpp
+++ Analysis_SCI_1Q/modcur_contour_gen/src/main.cpp
@@ -94,17 +94,9 @@ int main(int argc, char** argv) {
     }
 
     // calculate chi2 array
-    double min_chi2 = 10000000000;
     for (int i = 0; i < 45; i++) {
         for (int j = 0; j < 50; j++) {
             modcur_array_chi2[i][j] = modcur_meas.Chi2Test(&modcur_array[i][j], "WW CHI2");
-            if (modcur_array_chi2[i][j] < min_chi2) min_chi2 = modcur_array_chi2[i][j];
-        }
-    }
-    // subtract minimum chi2
-    for (int i = 0; i < 45; i++) {
-        for (int j = 0; j < 50; j++) {
-            modcur_array_chi2[i][j] -= min_chi2;
         }
     }
 
@@ -114,14 +106,54 @@ int main(int argc, char** argv) {
         cout << "output_file open failed: " << options_mgr.output_filename.Data() << endl;
         return 1;
     }
-    TH2D* delta_chi2_map = new TH2D("delta_chi2_map", "delta_chi2_map", 50, 0, 100, 45, 0, 180);
+    TH2D* chi2_map = new TH2D("chi2_map", "chi2_map", 50, 0, 100, 45, 0, 180);
     for (int i = 0; i < 45; i++) {
         for (int j = 0; j < 50; j++) {
-            delta_chi2_map->SetBinContent(j + 1, i + 1, modcur_array_chi2[i][j]);
+            chi2_map->SetBinContent(j + 1, i + 1, modcur_array_chi2[i][j]);
         }
     }
+    TH2D* chi2_map_inter = new TH2D("chi2_map_inter", "chi2_map_inter", 100, 0, 100, 180, 0, 180);
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 180; j++) {
+            double x = i + 0.5;
+            double y = j + 0.5;
+            double binc = chi2_map->Interpolate(x, y);
+            chi2_map_inter->SetBinContent(i + 1, j + 1, binc);
+        }
+    }
+    double min_chi2 = 10000000000;
+    double min_pol_degree = 0;
+    double min_pol_direction = 0;
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 180; j++) {
+            double binc = chi2_map_inter->GetBinContent(i + 1, j + 1);
+            if (binc < min_chi2) {
+                min_chi2 = binc;
+                min_pol_degree = i + 0.5;
+                min_pol_direction = j + 0.5;
+            }
+        }
+    }
+
+    cout << "========================================" << endl;
+    cout << "min_chi2 = " << min_chi2 << endl;
+    cout << "min_pol_degree = " << min_pol_degree << endl;
+    cout << "min_pol_direction = " << min_pol_direction << endl;
+    cout << "========================================" << endl;
+
+    TH2D* delta_chi2_map_inter = new TH2D("delta_chi2_map_inter", "delta_chi2_map_inter", 100, 0, 100, 180, 0, 180);
+
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 180; j++) {
+            double binc = chi2_map_inter->GetBinContent(i + 1, j + 1) - min_chi2 + 1;
+            delta_chi2_map_inter->SetBinContent(i + 1, j + 1, binc);
+        }
+    }
+
     output_file->cd();
-    delta_chi2_map->Write();
+    chi2_map->Write();
+    chi2_map_inter->Write();
+    delta_chi2_map_inter->Write();
 
     output_file->Close();
     delete output_file;
