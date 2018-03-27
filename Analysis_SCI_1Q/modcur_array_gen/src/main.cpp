3814a04c988245120f7a73f2bf9e4772800e17e4
Zhengheng Li <zhenghenge@gmail.com>
2018-03-27 12:57:52 +0800

    modcur_array_gen

--- Analysis_SCI_1Q/modcur_array_gen/src/main.cpp
+++ Analysis_SCI_1Q/modcur_array_gen/src/main.cpp
@@ -7,7 +7,27 @@
 #include "BarPos.hpp"
 #include "PosConv.hpp"
 
+/////////////////////////////////////////////////////////////
+// MODULATION CURVE ARRAY
+/////////////////////////////////////////////////////////////
+//
+// pol_direction: h1(45, 0, 180) => 2, 6, 10, ... , 174, 178
+//
+// pol_degree: h1(50, 0, 100) => 1, 3, 5, 7, 9, ... , 97, 99
+//
+/////////////////////////////////////////////////////////////
+
 using namespace std;
+
+int extract_pol_direction(const char* filename) {
+    string filename_str = filename;
+    size_t start_pos = filename_str.length() - 12;
+    string pol_direction_str = filename_str.substr(start_pos, 3);
+    cout << pol_direction_str << endl;
+
+    return 0;
+}
+
 
 int main(int argc, char** argv) {
     OptionsManager options_mgr;
@@ -20,8 +40,11 @@ int main(int argc, char** argv) {
         return 2;
     }
 
+
+
     for (size_t i = 0; i < options_mgr.pol_modcur_filelist.size(); i++) {
         cout << options_mgr.pol_modcur_filelist[i] << endl;
+        extract_pol_direction(options_mgr.pol_modcur_filelist[i].Data());
     }
 
 
